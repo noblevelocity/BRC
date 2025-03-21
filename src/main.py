4170dab2ba4368bf17c10f7e0a7747a461ff25dc
Maharshi Pandya <maharshipandya@Maharshis-MacBook-Air.local>
2025-03-22 03:48:16 +0530

    change

--- src/main.py
+++ src/main.py
@@ -1,35 +1,46 @@
 import math
 
 def main(input_file_name="testcase.txt", output_file_name="output.txt"):
-    input_file = open(input_file_name, "r")
-    output_file = open(output_file_name, "w")
-    minmap = {}
-    maxmap = {}
-    meanmap = {}
-    for line in input_file:
-        li = line.strip().split(';')
-        key, value = li[0], float(li[1])
-        if key in minmap:
-            minmap[key] = min(minmap[key], value)
-        else:
-            minmap[key] = value
-        if key in maxmap:
-            maxmap[key] = max(maxmap[key], value)
-        else:
-            maxmap[key] = value
-        if key in meanmap:
-            meanmap[key][0] += value
-            meanmap[key][1] += 1
-        else:
-            meanmap[key] = [value, 1]
-    for key in meanmap:
-        meanmap[key] = meanmap[key][0] / meanmap[key][1]
-    for key in sorted(minmap.keys()):
-        min_temp = math.ceil(minmap[key] * 10) / 10
-        mean_temp = math.ceil(meanmap[key] * 10) / 10
-        max_temp = math.ceil(maxmap[key] * 10) / 10
-        output_file.write(f"{key}={min_temp}/{mean_temp}/{max_temp}\n")
-    output_file.close()
-    input_file.close()
+    min_values = {}
+    max_values = {}
+    sums = {}
+    counts = {}
+    
+    with open(input_file_name, "r") as input_file:
+        for line in input_file:
+            key, value_str = line.strip().split(';', 1)
+            value = float(value_str)
+            
+            try:
+                if value < min_values[key]:
+                    min_values[key] = value
+            except KeyError:
+                min_values[key] = value
+                
+            try:
+                if value > max_values[key]:
+                    max_values[key] = value
+            except KeyError:
+                max_values[key] = value
+                
+            try:
+                sums[key] += value
+                counts[key] += 1
+            except KeyError:
+                sums[key] = value
+                counts[key] = 1
+    
+    sorted_keys = sorted(min_values.keys())
+    
+    results = []
+    for key in sorted_keys:
+        min_temp = math.ceil(min_values[key] * 10) / 10
+        mean_temp = math.ceil((sums[key] / counts[key]) * 10) / 10
+        max_temp = math.ceil(max_values[key] * 10) / 10
+        results.append(f"{key}={min_temp}/{mean_temp}/{max_temp}\n")
+    
+    with open(output_file_name, "w") as output_file:
+        output_file.writelines(results)
+
 if __name__ == "__main__":
     main()
