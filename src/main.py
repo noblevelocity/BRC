b49fd28f2a7f1f11c14dacc679f80b656f65b1e8
Maharshi Pandya <maharshipandya@Maharshis-MacBook-Air.local>
2025-03-22 03:01:39 +0530

    change

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import math
 from collections import defaultdict
 
-def process_file(input_file_name="testcase.txt", output_file_name="output.txt"):
+def main(input_file_name="testcase.txt", output_file_name="output.txt"):
     values = defaultdict(lambda: [float('inf'), float('-inf'), 0, 0])
 
     with open(input_file_name, "r") as input_file:
         for line in input_file:
-            key, value = line.strip().split(';')  
-            value = float(value) 
+            key, value = line.strip().split(';')
+            value = float(value)
 
             values[key][0] = min(values[key][0], value) 
             values[key][1] = max(values[key][1], value) 
@@ -17,9 +17,9 @@ def process_file(input_file_name="testcase.txt", output_file_name="output.txt"):
     with open(output_file_name, "w") as output_file:
         for key in sorted(values.keys()):
             minX = math.ceil(values[key][0] * 10) / 10
-            meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10 if values[key][3] > 0 else 0
-            maxX = math.ceil(values[key][1] * 10) / 10 
+            meanX = math.ceil((values[key][2] / values[key][3]) * 10) / 10
+            maxX = math.ceil(values[key][1] * 10) / 10
             output_file.write(f"{key}={minX}/{meanX}/{maxX}\n")
 
 if __name__ == "__main__":
-    process_file("testcase.txt", "output.txt")
+    main()
