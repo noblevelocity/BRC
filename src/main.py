0aee41d16239624dc5e21d1ece6064b0b83742bd
Maharshi Pandya <maharshipandya@Maharshis-MacBook-Air.local>
2025-03-21 22:29:28 +0530

    First change

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ def main(input_file_name = "testcase.txt", output_file_name = "output.txt"):
     first_line = input_file.readline().strip()
     first_line = first_line.split(";")
 
+
+
     output_file.write(f"{first_line[0]}={first_line[1]}/{1}/{1}\n")
 
     output_file.close()
