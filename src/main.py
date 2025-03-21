beb9b95329d6649c9cf750ebb6882fbc9bb0ecd6
Maharshi Pandya <maharshipandya@Maharshis-MacBook-Air.local>
2025-03-22 03:54:49 +0530

    change

--- src/main.py
+++ src/main.py
@@ -6,41 +6,38 @@ def main(input_file_name="testcase.txt", output_file_name="output.txt"):
     sums = {}
     counts = {}
     
-    with open(input_file_name, "r") as input_file:
-        for line in input_file:
-            key, value_str = line.strip().split(';', 1)
-            value = float(value_str)
-            
-            try:
-                if value < min_values[key]:
-                    min_values[key] = value
-            except KeyError:
-                min_values[key] = value
-                
-            try:
-                if value > max_values[key]:
-                    max_values[key] = value
-            except KeyError:
-                max_values[key] = value
-                
-            try:
-                sums[key] += value
-                counts[key] += 1
-            except KeyError:
-                sums[key] = value
-                counts[key] = 1
+    with open(input_file_name, "r") as f:
+        lines = f.readlines()
     
-    sorted_keys = sorted(min_values.keys())
+    for line in lines:
+        parts = line.strip().split(';', 1)
+        key = parts[0]
+        value = float(parts[1])
+        
+        try:
+            if value < min_values[key]: min_values[key] = value
+        except: min_values[key] = value
+        
+        try:
+            if value > max_values[key]: max_values[key] = value
+        except: max_values[key] = value
+        
+        try:
+            sums[key] += value
+            counts[key] += 1
+        except:
+            sums[key] = value
+            counts[key] = 1
     
     results = []
-    for key in sorted_keys:
-        min_temp = math.ceil(min_values[key] * 10) / 10
-        mean_temp = math.ceil((sums[key] / counts[key]) * 10) / 10
-        max_temp = math.ceil(max_values[key] * 10) / 10
-        results.append(f"{key}={min_temp}/{mean_temp}/{max_temp}\n")
+    for key in sorted(min_values):
+        min_t = math.ceil(min_values[key] * 10) / 10
+        mean_t = math.ceil((sums[key] / counts[key]) * 10) / 10
+        max_t = math.ceil(max_values[key] * 10) / 10
+        results.append(f"{key}={min_t}/{mean_t}/{max_t}\n")
     
-    with open(output_file_name, "w") as output_file:
-        output_file.writelines(results)
+    with open(output_file_name, "w") as f:
+        f.writelines(results)
 
 if __name__ == "__main__":
     main()
