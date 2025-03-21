bc32b7ea6a0201b50775e94a9afc0e58fc0d79df
Maharshi Pandya <maharshipandya@Maharshis-MacBook-Air.local>
2025-03-22 03:38:21 +0530

    change

--- src/main.py
+++ src/main.py
@@ -3,11 +3,9 @@ import math
 def main(input_file_name="testcase.txt", output_file_name="output.txt"):
     input_file = open(input_file_name, "r")
     output_file = open(output_file_name, "w")
-
     minmap = {}
     maxmap = {}
     meanmap = {}
-
     for line in input_file:
         li = line.strip().split(';')
         key, value = li[0], float(li[1])
@@ -24,19 +22,14 @@ def main(input_file_name="testcase.txt", output_file_name="output.txt"):
             meanmap[key][1] += 1
         else:
             meanmap[key] = [value, 1]
-
     for key in meanmap:
         meanmap[key] = meanmap[key][0] / meanmap[key][1]
-
     for key in sorted(minmap.keys()):
         min_temp = math.ceil(minmap[key] * 10) / 10
         mean_temp = math.ceil(meanmap[key] * 10) / 10
         max_temp = math.ceil(maxmap[key] * 10) / 10
         output_file.write(f"{key}={min_temp}/{mean_temp}/{max_temp}\n")
-
     output_file.close()
     input_file.close()
-
-
 if __name__ == "__main__":
     main()
